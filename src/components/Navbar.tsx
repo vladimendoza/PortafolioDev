f11fbeba1dc260d2f7d599548aec172b5bbb99b8
Bladimir Mendoza <tobysiezar@gmail.com>
2023-03-01 12:04:00 -0600

    navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,36 +5,39 @@ import Box from '@mui/material/Box';
 import useScrollTrigger from '@mui/material/useScrollTrigger';
 import Slide from '@mui/material/Slide';
 import DrawerComponent from '@/components/Drawer';
+import Image from 'next/image'
+import ByVladimir from '../../public/image/Logo/ByVladimir.svg'
+
 
 
 interface Props {
     window?: () => Window;
     children: React.ReactElement;
-  }
-  
-  //Funcion para Sticky Header
-  function HideOnScroll(props: Props) {
+}
+
+//Funcion para Sticky Header
+function HideOnScroll(props: Props) {
     const { children, window } = props;
     const trigger = useScrollTrigger({
-      target: window ? window() : undefined,
+        target: window ? window() : undefined,
     });
     return (
-      <Slide appear={false} direction="down" in={!trigger}>
-        {children}
-      </Slide>
+        <Slide appear={false} direction="down" in={!trigger}>
+            {children}
+        </Slide>
     );
-  }
+}
 export default function Navbar(props?: any) {
-  return (
-    <Box sx={{ display: 'flex' }}>
-    <HideOnScroll {...props}>
-      <AppBar component="nav" sx={{ background: 'white' }}>
-        <Toolbar sx={{ justifyContent: 'space-between', borderBottom: 1, borderColor: 'divider' }}>
-          <h1>Logo Here</h1>
-          <DrawerComponent />
-        </Toolbar>
-      </AppBar>
-    </HideOnScroll>
-  </Box>
-  )
+    return (
+        <Box sx={{ display: 'flex' }}>
+            <HideOnScroll {...props}>
+                <AppBar component="nav" sx={{ background: 'white' }}>
+                    <Toolbar sx={{ justifyContent: 'space-between', borderBottom: 1, borderColor: 'divider' }}>
+                         <Image src={ByVladimir} width={150} height={0} alt={'Logo By Vladimir'}/>
+                        <DrawerComponent />
+                    </Toolbar>
+                </AppBar>
+            </HideOnScroll>
+        </Box>
+    )
 }
--- src/screen/dashboard.tsx
+++ src/screen/dashboard.tsx
@@ -22,7 +22,7 @@ export const Dashboard = () => {
 
       <React.Fragment>
         <CssBaseline />
-        <Container>
+        <Container sx={{ mt: 15}}>
           <Grid container spacing={{ xs: 2, md: 2 }} columns={{ xs: 4, sm: 8, md: 12 }} style={{ background: 'red' }}
             direction="row"
             justifyContent="space-between"
